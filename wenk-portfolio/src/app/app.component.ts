02125528dbad5255d7a2a6b31c51a3d99c49982d
Silvan Wenk <silvan.wenk@ti8m.ch>
2024-09-04 09:35:40 +0200

    Adds introduction section

--- wenk-portfolio/src/app/app.component.ts
+++ wenk-portfolio/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { ApiService } from "./api.service";
+import { IntroductionComponent } from "./introduction/introduction.component";
+import { ProjectsComponent } from "./projects/projects.component";
+import { CareerComponent } from "./career/career.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IntroductionComponent, ProjectsComponent, CareerComponent],
   templateUrl: "./app.component.html",
-  styleUrl: "./app.component.less"
+  styleUrl: "./app.component.css"
 })
 export class AppComponent implements OnInit {
 
---
+++ wenk-portfolio/src/app/career/career.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'career',
+  standalone: true,
+  imports: [],
+  templateUrl: './career.component.html',
+  styleUrl: './career.component.css'
+})
+export class CareerComponent {
+
+}
