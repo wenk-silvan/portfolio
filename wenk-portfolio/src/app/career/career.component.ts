ae118adf9099b42483b8490540775254c8562e71
Silvan Wenk <silvan.wenk@ti8m.ch>
2024-09-04 18:00:46 +0200

    Adds career section

--- wenk-portfolio/src/app/career/career.component.ts
+++ wenk-portfolio/src/app/career/career.component.ts
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './career.component.css'
 })
 export class CareerComponent {
-
+  career = [
+    { "label": "Today", "text": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Labore et dolore magna aliquyam erat, sed diam voluptua." },
+    { "label": "2023", "text": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Labore et dolore magna aliquyam erat, sed diam voluptua." },
+    { "label": "2022", "text": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Labore et dolore magna aliquyam erat, sed diam voluptua." },
+    { "label": "2021", "text": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Labore et dolore magna aliquyam erat, sed diam voluptua." },
+  ]
 }
