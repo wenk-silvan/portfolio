6c6119b64b62b097e731df1a78653720ac9de7e7
Silvan Wenk <silvan.wenk@ti8m.ch>
2024-10-21 10:48:26 +0200

    Adds firestore to project and migrates introduction to firebase

--- src/app/app-data.model.ts
+++ src/app/app-data.model.ts
@@ -1,13 +1,11 @@
 import { Blogs } from "./home/blogs/blogs.model"
 import { Career } from "./home/career/career.model"
-import { Introduction } from "./home/introduction/introduction.model"
 import { Photography } from "./home/photography/photography.model"
 import { Projects } from "./home/projects/projects.model"
 
 export class AppData {
     career: Career = new Career()
     projects: Projects = new Projects()
-    introduction: Introduction = new Introduction()
     photography: Photography = new Photography()
     blogs: Blogs = new Blogs()
     link_back_text: string = ""
--- src/app/home/introduction/introduction.component.ts
+++ src/app/home/introduction/introduction.component.ts
@@ -1,15 +1,30 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { Introduction } from './introduction.model';
+import { map, Observable } from 'rxjs';
+import { Firestore, doc, docData } from '@angular/fire/firestore';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'introduction',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './introduction.component.html',
   styleUrl: './introduction.component.css'
 })
 export class IntroductionComponent {
+  introduction$: Observable<Introduction>
+  firestore: Firestore = inject(Firestore)
 
-  @Input() data: Introduction = new Introduction()
-
+  constructor() {
+    const introductionDoc = doc(this.firestore, 'introduction', 'jK92xD6avOVRczOtif7p').withConverter({
+      fromFirestore: (snapshot) => {
+        const data = snapshot.data();
+        return new Introduction(data['portrait_path'], data['icons'], data['description']);
+      },
+      toFirestore: (intro: Introduction) => ({ 
+        portrait_path: intro.portrait_path, icons: intro.icons, description: intro.description 
+      }),
+    });
+    this.introduction$ = docData<Introduction>(introductionDoc).pipe(map(intro => intro ?? new Introduction()))
+  }
 }
