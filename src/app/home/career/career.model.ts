c4ba7fab40b59ce1668d55c3e4f9a47556149b1d
Silvan Wenk <silvan.wenk@ti8m.ch>
2024-10-21 15:00:37 +0200

    Migrates career to firebase

--- src/app/home/career/career.model.ts
+++ src/app/home/career/career.model.ts
@@ -1,9 +1,6 @@
-export class Career {
-    title: string = ""
-    career: CareerItem[] = []
-}
-
 export class CareerItem {
-    label: string = ""
-    text: string = ""
+    constructor(
+        public label: string = "",
+        public text: string = ""
+    ) {}
 }
