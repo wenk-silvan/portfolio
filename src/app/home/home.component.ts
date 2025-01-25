963fa8f53f171f5cc9a815c0c829a21309509fc5
Silvan Wenk <silvan.wenk@gmail.com>
2025-01-25 18:06:12 +0100

    Adds contact section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,11 +4,12 @@ import { ProjectsComponent } from "./projects/projects.component";
 import { CareerComponent } from "./career/career.component";
 import { PhotographyComponent } from "./photography/photography.component";
 import { BlogsComponent } from "./blogs/blogs.component";
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [IntroductionComponent, ProjectsComponent, CareerComponent, PhotographyComponent, BlogsComponent],
+  imports: [IntroductionComponent, ContactComponent, ProjectsComponent, CareerComponent, PhotographyComponent, BlogsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
